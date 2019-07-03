2c53ccea6545b921930e3fd3737b6c04412ab2bf
Miles Wu <mileswu13@gmail.com>
2019-07-03 09:26:37 -0700

    Created config class

---
+++ classes/Config.php
@@ -0,0 +1,21 @@
+<?php
+class Config {
+    public static function get($path = null) {
+        if ($path) {
+            $config = $GLOBALS['config'];
+            $path = explode('/', $path) //returns array of elements separated by '/'
+            //print_r($path); prints array
+
+            foreach($path as $bit) {
+                if(isset($config[$bit])) {
+                    //following the next portion of path if path exists
+                    $config = $config[$bit]; 
+                }
+            }
+
+            return $config
+        }
+
+        return false;
+    }
+}
