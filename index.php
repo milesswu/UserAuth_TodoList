3afc681d9b58b68107d1d6169f0dd75b068fa170
Miles Wu <mileswu13@gmail.com>
2019-07-03 09:30:00 -0700

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 require_once 'core/init.php'
 
+//this function will load according to specified host without needing to directly acces the globals array
+var_dump(Config::get('mysql/host')); 
 ?>
