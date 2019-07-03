229a37e7a3d317694ddb50fb58d271e1089deafb
Miles Wu <mileswu13@gmail.com>
2019-07-03 10:18:46 -0700

    Implemented database class

---
+++ classes/DB.php
@@ -0,0 +1,32 @@
+<?php
+class DB {
+    private static $_instance = null;
+    private $_pdo,              //stores instantiated pdo object
+            $_query,            //store last query executed
+            $_error = false,    //whether there has been an error
+            $_results,          //result set
+            $_count = 0;        //number of results
+
+    //Connects to database on construction
+    private function __construct() {
+        try {
+            //PDO object takes host and database string, username, password
+            $this->$_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'),
+                Config::get('mysql/username'),
+                Config::get('mysql/password')); 
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    //Returns a new instance of the database connection if does not already exist
+    //Otherwise, simply returns existing instance (no need to reconnect)
+    public static function getInstance() {
+        if (!isset(self::$_instance)) {
+            self::$_instance = new DB();
+        }
+        return self::$_instance;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -3,4 +3,14 @@ require_once 'core/init.php';
 
 //this function will load according to specified host without needing to directly acces the globals array
 var_dump(Config::get('mysql/host')); 
+
+//Example of how we might use the DB class to easily access database through instances
+/*
+    $users = DB::getInstance()->query('SELECT username FROM users');
+    if ($users->count()) {
+        foreach($users as $user) {
+            echo $user->$username;
+        }
+    }
+*/
 ?>
